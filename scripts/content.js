3754cb1e16c4eada53379670aae1979fbe7f2889
henry <henry@incuna.com>
2012-11-25 16:55:57 +0000

    Reduce columns to 3 abreast

--- scripts/content.js
+++ scripts/content.js
@@ -1,6 +1,9 @@
 jQuery(document).ready(function($) {
     var content = $('section article');
-    for (var i = 0; i < 5; i++) {
+    for (var i = 0; i < 2; i++) {
         content.clone().appendTo($('section'));
     }
+    $('.go').click(function() {
+        $('.readless').readless()
+    })
 });
